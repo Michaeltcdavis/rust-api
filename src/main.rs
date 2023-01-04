cea494bab78a26d3aed8c06fcbed95e655ad42a9
Michael Davis <michaeltcdavis@gmail.com>
2023-01-03 20:31:09 -0500

    add state to application route

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,24 @@
 
   use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
 
-  async fn index() -> impl Responder {
-      HttpResponse::Ok().body("Hello world!")
+  struct AppState {
+    app_name: String,
   }
 
-  #[post("/echo")]
-  async fn echo(req_body: String) -> impl Responder {
-      HttpResponse::Ok().body(req_body)
-  }
-
-  async fn manual_hello() -> impl Responder {
-      HttpResponse::Ok().body("Hey there!")
+  #[get("/")]
+  async fn index(data: web::Data<AppState>) -> String {
+    let app_name = &data.app_name;
+      format!("Hello {app_name}!")
   }
 
   #[actix_web::main]
   async fn main() -> std::io::Result<()> {
       HttpServer::new(|| {
           App::new()
-            .service(echo)
-            .service(
-                web::scope("/app")
-                    .route("/index.html", web::get().to(index))
-            )
+            .app_data(web::Data::new(AppState {
+              app_name: String::from("Actix Web"),
+            }))
+            .service(index)
       })
       .bind(("127.0.0.1", 8080))?
       .run()
